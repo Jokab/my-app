0df275df329882a5ca06aae99090c9f701c54fe9
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2021-05-23 22:44:29 +0200

    Show late departures

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {LineNumber} from './LineNumber';
 import './App.css';
 import React, { Component } from 'react';
+import {DateTime} from 'luxon';
 
 type AppState = {
   departures: any;
@@ -20,13 +21,28 @@ type DepartureProps = {
 
 class Departure extends Component<DepartureProps, {}> {
   render() {
+    let minutesLate = this.calculateHowLate(this.props.departure.time, this.props.departure.rtTime)
+
+    const departureTime = this.props.departure.rtTime ? this.props.departure.rtTime : this.props.departure.time;
+    let timeCell;
+    if (minutesLate > 0) {
+      timeCell = <td>{departureTime} (+{minutesLate})</td>
+    } else if (minutesLate < 0) {
+      timeCell = <td>{departureTime} (-{minutesLate})</td>
+    } else {
+      timeCell = <td>{departureTime}</td>
+    }
     return (
       <tr>
           <td className="LineNumberCell"><LineNumber line={this.props.departure.sname}/></td>
           <td>{this.props.departure.direction}</td>
-          <td>{this.props.departure.time}</td>
+          {timeCell}
       </tr>
     )
+  }
+
+  calculateHowLate(time: string, rtTime: string): number {
+    return DateTime.fromFormat(rtTime, "HH:mm").diff(DateTime.fromFormat(time, "HH:mm")).minutes;
   }
 }
 
